d7164f4f871e01f8b4c20e3049551d8aa73b3649
Calvin Chen <calvinxchen0824@gmail.com>
2026-02-25 20:35:50 -0500

    fixed pack tests

--- Pack_tests.cpp
+++ Pack_tests.cpp
@@ -12,6 +12,47 @@ TEST(test_pack_default_ctor) {
     ASSERT_EQUAL(SPADES, first.get_suit());
 }
 
+TEST(pack_istream){
+    istringstream i("Ten of Spades");
+    Pack pack(i);
+    Card first = pack.deal_one();
+    ASSERT_EQUAL(TEN, first.get_rank());
+    ASSERT_EQUAL(SPADES, first.get_suit());
+}
+
+TEST(resetNext) {
+    Pack pack;
+    Card first = pack.deal_one();
+    pack.reset();
+    first = pack.deal_one();
+    ASSERT_EQUAL(NINE, first.get_rank());
+    ASSERT_EQUAL(SPADES, first.get_suit());
+}
+
+TEST(test_shuffle) {
+    Pack pack;
+    pack.shuffle();
+    Card first = pack.deal_one();
+    Card second = pack.deal_one();
+    ASSERT_EQUAL(JACK, second.get_rank());
+    ASSERT_EQUAL(SPADES, second.get_suit());
+    ASSERT_EQUAL(KING, first.get_rank());
+    ASSERT_EQUAL(SPADES, first.get_suit());
+}
+
+TEST(pack_istream1){
+    Pack pack;
+    for(int i =0 ; i < 24; ++i){
+        pack.deal_one();
+    }
+    ASSERT_TRUE(pack.empty());
+}
+
+TEST(pack_istream2){
+    Pack pack;
+    ASSERT_FALSE(pack.empty());
+}
+
 // Add more tests here
 
 TEST_MAIN()
